20c0d319b22b109ba0f1946249b067ae5b13d292
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-20 15:06:02 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,8 @@
-require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
   def get_genre_name
-    #binding.pry 
     self.genre.name 
   end
 
@@ -12,14 +10,7 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
-    #binding.pry 
-    Artist.where(name: "Drake").first_or_create do |artist|
-      artist.name = "Drake"
-    end 
-    
-  #   MyStat.where(name: statistic_name).first_or_create do |statistic|
-  # statistic.value = calculate_percentage
-  # statistic.statistic_type = "percentage"
-    
+    self.artist = Artist.find_or_create_by(name: "Drake") 
+
   end
 end
