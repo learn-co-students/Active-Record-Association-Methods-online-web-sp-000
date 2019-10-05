bd5691c38c0711a895378048cd5dee988c92f0dc
Oana Berentan <oana.berentan@yahoo.com>
2019-10-05 16:34:07 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,12 +3,10 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
     self.songs.count
   end
 
   def artist_count
-    # return the number of artists associated with the genre
     self.artists.count
   end
 
