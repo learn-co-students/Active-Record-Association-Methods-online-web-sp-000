a97dcc0d9577aa024868b7a859fb57191f22732b
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-01-12 12:00:25 +0000

    added comments

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,15 +2,27 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def get_first_song
+    #we call songs on the instance that the method will be called on in the future by using self!
+    #self.songs will return array of the artist's  songs.  For the first song, we just have to chain on a first.
+    self.songs.first
+  end
+
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    get_first_song.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count 
+    #self.songs.length
+
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.count
+    #self.genres.length
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,18 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.artists.collect{|artist| artist.name}
+    #self.artists.map(&:name)
+    #tags.map(&:name) is sort from of tags.map{|s| s.name}
   end
 end
