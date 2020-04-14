34595bc83d5055cfff8cae358042954292313b88
dayofgray <graysonpberger@gmail.com>
2020-04-13 21:32:04 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,17 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
-
   def get_genre_name
     self.genre.name
   end
 
   def drake_made_this
-    if Artist.find_by(name:'Drake')
-      self.artist = Artist.find_by(name:'Drake')
-    else
-      drake = Artist.create(name:'Drake')
-      self.artist = drake
-    end
+    artist_d = Artist.find_or_create_by(name: "Drake")
+    self.artist = artist_d
   end
 end
