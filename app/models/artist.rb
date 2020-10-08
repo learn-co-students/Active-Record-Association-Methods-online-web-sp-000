8794203ba30d8c4b3a90244bfec698e35020efd7
nlanese21 <nlanese21@gmail.com>
2020-10-07 21:40:44 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,29 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    selected = Song.find(1)
+    return selected.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    byArtist = 0
+    Song.all.each do | selected |
+      if selected.artist == self
+        byArtist += 1
+      end
+    end
+    return byArtist
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    genresHad = 0
+    Genre.all.each do | selected |
+      if selected.artists.include?(self)
+        genresHad += 1
+      end
+    end
+    return genresHad
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,35 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    songsInGenre = 0
+    Song.all.each do | selected |
+      if selected.genre == self
+        songsInGenre += 1
+      end
+    end
+    return songsInGenre 
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    artistsOfGenre = 0
+    Artist.all.each do | selected | 
+      if selected.genres.include?(self)
+        artistsOfGenre += 1
+      end
+    end
+    return artistsOfGenre
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artistNames = []
+    Artist.all.each do | selected |
+      if selected.genres.include?(self)
+        artistNames << selected.name
+      end
+    end
+    return artistNames
   end
+
 end
